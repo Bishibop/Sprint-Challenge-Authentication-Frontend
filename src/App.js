03c74c84b3fade8c6fbf98bd99c6c99e3320ba17
Nicholas Mullen <nwmullen@gmail.com>
2020-06-12 23:55:04 -0400

    tweak

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Jokes from './components/Jokes';
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   return (
-    // No time for separate pages! But did all register, login, and joke
-    // getting functionality.
     <div className="App">
       {!loggedIn && 
         <>
